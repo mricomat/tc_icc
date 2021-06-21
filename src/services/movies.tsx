a8f05a7fe3d00aca02f9444f8671f50ee09120fd
mricomat <maartinricomartinez@gmail.com>
2021-06-21 15:18:24 +0200

    Removed service folder

--- src/services/movies.tsx
+++
@@ -1,4 +0,0 @@
-import { get } from 'src/services/fetch';
-import { IMovie } from 'src/types/data';
-
-export const searchMovies = async (query: string, page: number = 1) => get<IMovie>(`/search/movie`, { query, page });
